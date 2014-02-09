abf8d8be877b098aae64d6034789521f72dc148e
Martin Hardselius <martin.hardselius@gmail.com>
2014-02-09 16:07:18 +0100

    Implemented dynamic filtering of results

--- public/knockout/script.js
+++ public/knockout/script.js
@@ -1,12 +1,23 @@
-var MedalModel = function() {
+var MedalModel = function () {
     var self = this;
+
     self.medals = ko.observableArray();
-    self.getMedals = function() {
+    self.filter = ko.observable();
+
+    self.getMedals = function () {
         $.getJSON('/medals', function (data) {
             console.log(data);
             self.medals(data);
         });
     }.bind(self);
+
+    self.medalsToShow = ko.computed(function () {
+        var filterString = self.filter();
+        if (filterString === '' || filterString === undefined) return self.medals();
+        return ko.utils.arrayFilter(self.medals(), function (item) {
+            return item.name.toLowerCase().indexOf(filterString.toLowerCase()) !== -1;
+        });
+    }, this);
 };
 
 ko.applyBindings(new MedalModel());
