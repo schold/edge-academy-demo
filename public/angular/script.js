7dbf5f743ed95f60ec731e89bbfb0f8f9f4a4116
Viktor Mazetti <viktor.mazetti@netlight.com>
2014-02-10 23:34:40 +0100

    Added angular implementation

--- public/angular/script.js
+++ public/angular/script.js
@@ -1,3 +1,34 @@
-/**
- * Created by martin on 08/02/14.
- */
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
+  	$scope.order = function(predicate) {
+  		if (predicate === $scope.predicate) {
+  			$scope.predicate = "-"+predicate;
+  		} else {
+  			$scope.predicate = predicate;
+  		}
+  	};
+
+  	$scope.getMedals = function() {
+		$http.get("/medals").success(function(data) {
+	  		$scope.data = data;
+	  	});
+  	};
+  	
+
+  }]);
